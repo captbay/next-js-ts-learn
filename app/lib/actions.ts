d651a1119cbae3da83ca8470910bda99c2251d5e
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-02-05 00:40:46 +0700

    revalidate for update new data from DB after insert or do something in DB

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 
 import { sql } from '@vercel/postgres';
 
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -27,4 +30,7 @@ export async function createInvoice(formData: FormData) {
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
