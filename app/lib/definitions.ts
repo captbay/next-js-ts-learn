c32e1d2299285e8c870420881ba0525600abfafc
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-02-05 00:39:09 +0700

    make form in server and validate form using zod

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -17,13 +17,11 @@ export type Customer = {
 };
 
 export type Invoice = {
-  id: string;
+  id: string; // Will be created on the database
   customer_id: string;
-  amount: number;
-  date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+  amount: number; // Stored in cents
   status: 'pending' | 'paid';
+  date: string;
 };
 
 export type Revenue = {
